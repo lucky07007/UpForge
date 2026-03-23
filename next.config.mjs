bf5341d481358023963c5ee42b5128873ff21adf
lucky07007 <lucky7840079250@gmail.com>
2026-03-23 14:29:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -34,7 +34,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.upforge.org",
+            value: 'https://www.upforge.org, https://www.upforge.in',
           },
           {
             key: "Access-Control-Allow-Methods",
