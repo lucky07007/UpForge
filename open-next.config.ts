b2f9eb8bcfbcaaab55f5cf60d1dc3d3229a90d05
lucky07007 <lucky7840079250@gmail.com>
2026-09-10 22:41:12 +0530

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-    },
-    bundler: {
-      externals: ["jose", "jwks-rsa", "firebase-admin"],
-    },
-  },
-});
+export default defineCloudflareConfig();
